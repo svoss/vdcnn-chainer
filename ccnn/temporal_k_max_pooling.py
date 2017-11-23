d4bff65ed61f668f4a82d90415290e32cdbda720
Stijn Voss <svoss@i-sti.nl>
2017-11-23 13:45:17 +0100

    Temporal max pooling gpu start

--- ccnn/temporal_k_max_pooling.py
+++ ccnn/temporal_k_max_pooling.py
@@ -1,6 +1,7 @@
 from chainer import Function
 import numpy as np
 
+from chainer import cuda
 
 class TemporalKMaxPooling(Function):
     """ The temporal K max pooling selects the k highest values of an input vector
@@ -12,29 +13,47 @@ class TemporalKMaxPooling(Function):
 
     def forward_cpu(self, x):
         x, = x
+        # Only works for two dimensional input for now
         assert len(x.shape) == 2, ("Temperal K max pooling only support 2d input, got %d" % len(x.shape))
         N, D = x.shape
         assert x.shape[1] >= self.k, "Input of temporal K-max pooling layer should be equal or higher then k (k=%d, %d given)" % (self.k , x.shape[1])
-        k = np.partition(x, D-self.k,axis=1)[:, D-self.k]
-        NE = np.ones(N,dtype=np.int32) * self.k
-        self.indexes = np.empty((N, self.k), dtype=np.int32)
-        vals = np.empty((N, self.k), dtype=x.dtype)
-        for n in xrange(N):
-            for i in xrange(D):
-                if x[n,i] > k[n]:
-                    NE[n] -= 1
-            ki = 0
 
+        k = np.partition(x, D-self.k, axis=1)[:, D-self.k:]
+        # k[n], will contain the k highest element but not necessarily in the correct order
+        # k[n][0] will be kth highest element
+
+        # keeps track of max number of elements that can be equal to the k-th element
+        # this will prevent the top-3 of [1,1,1,2] to be [1,1,1]
+        # begin by assuming all k elements are equal and we thus want k elements to be equal
+        ne = np.ones(N, dtype=np.int32) * self.k
+
+        # indexes and values of array
+        self.indexes = np.empty((N, self.k), dtype=np.int32) # indexes should be kept for back propagation
+        vals = np.empty((N, self.k), dtype=x.dtype)
+
+        for n in xrange(N):
+            # for every element after the kth element check if it's higher, a higher element means
+            # that we should select one item  equal to the  kth highest element less
+            for i in xrange(1, self.k):
+                if k[n, i] > k[n][0]:
+                    ne[n] -= 1
+
+            # select higher or equal to the k-th highest element in the order they exist in the data
+            ki = 0 # number of elements we already found
             for i in xrange(D):
-                if x[n,i] > k[n]:
+                # Higher then k-th element, select it anyway
+                if x[n, i] > k[n][0]:
                     self.indexes[n, ki] = i
                     vals[n, ki] = x[n,i]
                     ki += 1
-                if x[n,i] == k[n] and NE[n] > 0:
+
+                # If it's equal, check that we are not exceeding or maximum
+                if x[n, i] == k[n][0] and ne[n] > 0:
                     self.indexes[n, ki] = i
                     vals[n, ki] = x[n, i]
                     ki += 1
-                    NE[n] -= 1
+
+                    ne[n] -= 1  # One less to select
 
                 if ki >= self.k:
                     break
@@ -43,30 +62,75 @@ class TemporalKMaxPooling(Function):
     def backward_cpu(self, x, grad_outputs):
         x, = x
         gx = np.zeros(x.shape, dtype=x.dtype)
+
         gy, = grad_outputs
         for n in range(x.shape[0]):
-            gx[n,self.indexes[n]] = gy[n]
+            gx[n, self.indexes[n]] = gy[n]
+
         return gx,
 
+    def forward_gpu(self, x):
+        x, = x
+        # Only works for two dimensional input for now
+        assert len(x.shape) == 2, ("Temperal K max pooling only support 2d input, got %d" % len(x.shape))
+        N, D = x.shape
+        assert x.shape[
+                   1] >= self.k, "Input of temporal K-max pooling layer should be equal or higher then k (k=%d, %d given)" % (
+        self.k, x.shape[1])
+        # cupy.partition actually does full sort, but might improve in future
+        M = cuda.cupy.partition(x, D - self.k, axis=1)[:, D - self.k:]
+        # k[n], will contain the k highest element but not necessarily in the correct order
+        # k[n][0] will be kth highest element
+
+        # indexes and values of array
+        self.indexes = cuda.cupy.empty((N, self.k), dtype=np.int32)  # indexes should be kept for back propagation
+        y = cuda.cupy.empty((N, self.k), dtype=x.dtype)
+        cuda.elementwise(
+            'T x, T m, int32 k, int32 n',
+            'T out, int32 indexes',
+            '''
+                int ne = k;
+                for (int ki = 1; ki < k; k++) {
+                    if ( m[ki] > m[0] ) {
+                        ne--;
+                    }
+                }
+                
+                int ki = 0;
+                for (int d = 0; d < n; d++){
+                    if (x[d] > m[0]) {
+                        indexes[ki] = d;
+                        out[ki] = x[d];
+                        ki++;
+                    }
+                    
+                    if (x[d] == m[0] && ne > 0) {
+                        indexes[ki] = d;
+                        out[ki] = x[d];
+                        ki++;
+                        ne--;
+                    }
+                }
+            ''',
+            'temp_k_max_fwd'
+
+        )(M, x, self.k, D, y, self.indexes)
+
+        return y,
 
 def temporal_k_max_pooling(x, k):
     y = TemporalKMaxPooling(k)(x)
     return y
 
+
 if __name__ == '__main__':
-    from chainer import Variable
     x = np.random.randint(0, 9, size=(3, 100))
     x[0, [0, 5, 6, 35, 65]] = 11
     x[1] = [0 if i < 96 else 1 for i in xrange(100)]
     x[2, [1, 44, 55, 98]] = 10
     x[2, [4, 22, 99]] = 11
-    x = Variable(np.array(x, dtype=np.float32))
-    y = temporal_k_max_pooling(x, 5)
-
-
-    y.grad = np.ones((3,5), dtype=np.float32)
-    y.backward()
-    print x.grad
-
+    x = cuda.cupy.array(x, dtype=np.float64)
+    tmp = TemporalKMaxPooling(3)
+    tmp.forward_gpu((x,))
 
 
--- tests/test_temporal_k_max_pooling.py
+++ tests/test_temporal_k_max_pooling.py
@@ -6,30 +6,88 @@ sys.path.append("../ccnn/")
 from chainer import testing
 from chainer import gradient_check
 from ccnn.temporal_k_max_pooling import TemporalKMaxPooling, temporal_k_max_pooling
+import chainer
+from chainer import cuda
+
+from chainer.testing import attr
+@testing.parameterize(*testing.product({
+    'dtype': [np.float16, np.float32, np.float64],
+}))
+
 class TestTemporalKMaxPooling(unittest.TestCase):
-    def test_forward_cpu(self):
+
+    def setUp(self):
         x = np.random.randint(0, 9, size=(3, 100))
         x[0, [0, 5, 6, 35, 65]] = 11
         x[1] = [0 if i < 96 else 1 for i in xrange(100)]
         x[2, [1, 44, 55, 98]] = 10
         x[2, [4, 22, 99]] = 11
-        tmp = TemporalKMaxPooling(k=5)
-        x = np.array(x, dtype=np.float64)
-        y, = tmp.forward_cpu((x,))
-        self.assertTrue(np.array_equal(y[0], [11.0, 11.0, 11.0, 11.0, 11.0]))
-        self.assertTrue(np.array_equal(y[1], [0.0, 1.0, 1.0, 1.0, 1.0]))
-        self.assertTrue(np.array_equal(y[2],  [10.0, 11.0, 11.0, 10.0, 11.0]))
+        x = np.array(x, dtype=self.dtype)
+        self.forward_x = x # some specific edge cases for forward call
 
-    def test_backward_cpu(self):
+        self.backward_x = np.arange(3*25, dtype=self.dtype)
+        np.random.shuffle(self.backward_x)
+        self.backward_x = self.backward_x.reshape(3, 25)
+        self.y_grad = np.random.rand(3, 5).astype(self.dtype)
 
-        x = np.random.randn(3, 100).astype(np.float32)
+
+        # when dtype is float16, less accurate
+        if self.dtype == np.float16:
+            self.check_backward_options = {
+                'atol': 1e-3, 'rtol': 1e-2}
+            self.check_double_backward_options = {
+                'atol': 1e-3, 'rtol': 1e-2}
+        else:
+            self.check_backward_options = {
+                'atol': 1e-4, 'rtol': 1e-3}
+            self.check_double_backward_options = {
+                'atol': 1e-4, 'rtol': 1e-3}
+
+    def check_forward(self, x_data, use_cudnn='always'):
+
+        x = chainer.Variable(x_data)
+        with chainer.using_config('use_cudnn', use_cudnn):
+            y = temporal_k_max_pooling(x, 5)
+        y_data = y.data
+        self.assertEquals(self.dtype, y_data.dtype)
+
+        expect = np.array([
+            [11.0, 11.0, 11.0, 11.0, 11.0],
+            [0.0, 1.0, 1.0, 1.0, 1.0],
+            [10.0, 11.0, 11.0, 10.0, 11.0]
+            ], dtype=self.dtype)
+        testing.assert_allclose(expect, y_data)
+
+    def test_forward_cpu(self):
+        self.check_forward(self.forward_x)
+
+    def check_backward(self, x_data, y_grad, use_cudnn='always'):
 
         def f(x):
             return temporal_k_max_pooling(x, 5)
 
-        y_grad = np.random.randn(3, 5).astype(np.float32)
+        with chainer.using_config('use_cudnn', use_cudnn):
+            gradient_check.check_backward(f, x_data, y_grad, dtype='d', **self.check_backward_options)
 
-        gradient_check.check_backward(f, x, y_grad)
+    def test_backward_cpu(self):
+        try:
+            self.check_backward(self.backward_x, self.y_grad)
+        except AssertionError as e:
+            print(e)
 
-    #(y[0], [11.0, 11.0, 11.0, 11.0, 11.0])
+    @attr.gpu
+    def test_forward_gpu(self):
+        self.check_forward(cuda.to_gpu(self.x))
+
+    @attr.gpu
+    def test_forward_gpu_non_contiguous(self):
+        self.check_forward(cuda.cupy.asfortranarray(cuda.to_gpu(self.x)))
+
+    @attr.gpu
+    def test_forward_gpu_no_cudnn(self):
+        self.check_forward(cuda.to_gpu(self.x), 'never')
+
+            #(y[0], [11.0, 11.0, 11.0, 11.0, 11.0])
+
+
 testing.run_module(__name__, __file__)
