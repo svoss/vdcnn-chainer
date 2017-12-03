47fdf434d9773d5d27fce5492d0c0efbf64aeac3
svoss <svoss@i-sti.nl>
2017-12-03 13:45:51 +0100

    Delete runs.sh
    Added sh file by accident

--- runs.sh
+++
@@ -1,3 +0,0 @@
-env/bin/python ccnn/train.py --dataset=yelp-polarity --out=results/yelp-polarity --depth=29
-env/bin/python ccnn/train.py --dataset=yelp-full --out=results/yelp-full --depth=29
-env/bin/python ccnn/train.py --dataset=ag-news --out=results/ag-news --depth=29
